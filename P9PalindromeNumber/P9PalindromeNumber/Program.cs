fe1bc19fa04065d85fd112a689d2b2b5d4d64c93
Anton Isakov <tonias@yandex.ru>
2023-06-03 11:20:06 +0300

    the first try from mac

---
+++ P9PalindromeNumber/P9PalindromeNumber/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var solution = new Solution();
+
+Console.WriteLine(solution.IsPalindrome(12321));
+Console.WriteLine(solution.IsPalindrome(1111));
+Console.WriteLine(solution.IsPalindrome(-2112));
+Console.WriteLine(solution.IsPalindrome(0));
+Console.WriteLine(solution.IsPalindrome(15));
+Console.WriteLine(solution.IsPalindrome(Int32.MaxValue));
+Console.WriteLine(solution.IsPalindrome(Int32.MinValue));
+
+public class Solution {
+    public bool IsPalindrome(int x)
+    {
+        Console.WriteLine(x);
+        if (x < 0)
+            return false;
+
+        int digitsNumber = 0;
+        while (x>=Math.Pow(10, digitsNumber))
+        {
+            digitsNumber++;
+        }
+        
+        Console.WriteLine($"digitsNumber: {digitsNumber}");
+
+        int[] digits = new int[digitsNumber];
+        for (int i = 0; i < digits.Length; i++)
+        {
+            var delimiter = Math.Pow(10, i);
+            digits[i] = (int)((x % (delimiter*10)) /delimiter);
+        }
+
+        for (int i = 0; i < digits.Length/2; i++)
+        {
+            var a = digits[i];
+            var b = digits[digits.Length - 1 - i];
+            if (a != b)
+                return false;
+        }
+
+        return true;
+    }
+}
