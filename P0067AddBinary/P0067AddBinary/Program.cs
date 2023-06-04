cdb757d65b7d781615c5aa9307afaa6c16428fd5
Anton Isakov <tonias@yandex.ru>
2023-06-05 02:02:38 +0300

    2 more problems resolved

---
+++ P0067AddBinary/P0067AddBinary/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace P0067AddBinary
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(AddBinary("111", "100"));
+            Console.WriteLine(AddBinary("01010101010101", "1111111111111111"));
+            Console.WriteLine(AddBinary("11", "1111111111111111"));
+            Console.WriteLine(AddBinary(
+                "10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101",
+                "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011"));
+
+            Console.ReadLine();
+        }
+
+        public static string AddBinary(string a, string b)
+        {
+            string s1;
+            string s2;
+            if (a.Length > b.Length)
+            {
+                s1 = a;
+                s2 = b;
+            }
+            else
+            {
+                s1 = b;
+                s2 = a;
+            }
+            var result = string.Empty;
+            var up = false;
+            for (int i = 0; i < s1.Length; i++)
+            {
+                int bit1 = s1[s1.Length - 1 - i] == '1' ? 1 : 0;
+                int bit2 = 0;
+                if (s2.Length > i)
+                {
+                    bit2 = s2[s2.Length - 1 - i] == '1' ? 1 : 0;
+                }
+                int sum = bit1 + bit2;
+                if (up)
+                {
+                    sum++;
+                }
+
+                result = (sum % 2 == 1 ? "1" : "0") + result;
+                up = sum > 1;
+            }
+
+            if (up)
+            {
+                result = "1" + result;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ P0069SqrtX/P0069SqrtX/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace P0069SqrtX
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var solution = new Solution();
+            Console.WriteLine(solution.MySqrt(4));
+            Console.WriteLine(solution.MySqrt(65235));
+            Console.WriteLine(solution.MySqrt(20000000));
+            Console.WriteLine(solution.MySqrt(int.MaxValue));
+            Console.ReadLine();
+        }
+
+        public class Solution
+        {
+            static Solution()
+            {
+                for (uint i = 0; i < Squares.Length; i++)
+                {
+                    Squares[i] = i * i;
+                }
+            }
+
+            private static readonly uint[] Squares = new uint[46342];
+
+            public int MySqrt(int x)
+            {
+                for (int i = 0; i < Squares.Length - 1; i++)
+                {
+                    var s = Squares[i];
+                    if (x == s)
+                        return i;
+                    if (x > s && x < Squares[i + 1])
+                        return i;
+                }
+
+                return -1;
+            }
+        }
+    }
+}
