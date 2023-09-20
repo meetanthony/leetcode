5d99fb2cfabdd03fbd06cee3108a7e6cb0517c28
Anton Isakov <tonias@yandex.ru>
2023-09-20 19:05:33 +0300

    53. Maximum Subarray solved

---
+++ P0053MaximumSubarray/P0053MaximumSubarray/Caterpillar.cs
@@ -0,0 +1,24 @@
+﻿namespace P0053MaximumSubarray;
+
+internal static class Caterpillar
+{
+    public static int Solve(int[] nums)
+    {
+        int maxSum = Int32.MinValue;
+
+        int pointer = 0;
+        int sum = 0;
+
+        while (pointer < nums.Length)
+        {
+            sum += nums[pointer];
+            if (sum > maxSum)
+                maxSum = sum;
+            if (sum < 0)
+                sum = 0;
+            pointer++;
+        }
+
+        return maxSum;
+    }
+}
---
+++ P0053MaximumSubarray/P0053MaximumSubarray/Stupid.cs
@@ -0,0 +1,58 @@
+﻿namespace P0053MaximumSubarray;
+
+internal static class Stupid
+{
+    public static int Solve(int[] nums)
+    {
+        int maxSum = int.MinValue;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int accum = 0;
+            for (int j = i; j < nums.Length; j++)
+            {
+                accum += nums[j];
+                if (accum > maxSum)
+                {
+                    maxSum = accum;
+                }
+            }
+        }
+
+        return maxSum;
+    }
+
+    public static int Solve2(int[] nums)
+    {
+        int maxSum = int.MinValue;
+
+        for (int i = 0; i < nums.Length;)
+        {
+            int accum = 0;
+            for (int j = i; j < nums.Length; j++)
+            {
+                accum += nums[j];
+                if (accum > maxSum)
+                {
+                    maxSum = accum;
+                }
+            }
+
+            accum -= nums[i];
+            i++;
+
+            for (int j = nums.Length - 1; j >= i; j--)
+            {
+                if (accum > maxSum)
+                {
+                    maxSum = accum;
+                }
+                accum -= nums[j];
+            }
+
+            i++;
+        }
+
+        return maxSum;
+    }
+}
