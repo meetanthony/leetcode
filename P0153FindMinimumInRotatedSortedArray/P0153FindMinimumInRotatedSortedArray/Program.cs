d54dc933eb61468dbd3168488017931c9d078349
Anton Isakov <tonias@yandex.ru>
2023-09-22 08:28:45 +0300

    P0513 project template added

---
+++ P0153FindMinimumInRotatedSortedArray/P0153FindMinimumInRotatedSortedArray/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace P0153FindMinimumInRotatedSortedArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public interface IMinimumFinder
+    {
+        int FindMin(int[] nums);
+    }
+
+    public class Stupid : IMinimumFinder
+    {
+        public int FindMin(int[] nums)
+        {
+            var min = int.MinValue;
+            foreach (var num in nums)
+            {
+                if (num > min)
+                    min = num;
+            }
+            return min;
+        }
+    }
+}
