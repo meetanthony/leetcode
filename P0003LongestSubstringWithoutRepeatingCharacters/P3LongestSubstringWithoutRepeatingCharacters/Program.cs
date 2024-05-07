67343ca4deb2ad8e75b95dcb8d3c76cfed8c446e
Anton Isakov <tonias@yandex.ru>
2024-05-07 21:27:53 +0300

    Algo test with brute-force cases

--- P0003LongestSubstringWithoutRepeatingCharacters/P3LongestSubstringWithoutRepeatingCharacters/Program.cs
+++ P0003LongestSubstringWithoutRepeatingCharacters/P3LongestSubstringWithoutRepeatingCharacters/Program.cs
@@ -1,33 +1,50 @@
-﻿namespace P3LongestSubstringWithoutRepeatingCharacters;
+﻿using Common.TestData;
+using System.Text;
+
+namespace P3LongestSubstringWithoutRepeatingCharacters;
 
 internal class Program
 {
-
     private static void Main(string[] args)
     {
-        Test(new HashSetCounter());
-        Console.WriteLine();
-        Test(new DictionaryCounter());
+        var brutforceArrays = new BrutforceArrays<char>(10, 'a', 'd');
+        var casesCount = brutforceArrays.CasesCount;
+        Console.WriteLine($"brutforce cases: {casesCount}");
+
+        var stopUi = false;
+
+        var hashCounter = new HashSetCounter();
+        var dictCounter = new DictionaryCounter();
+
+        long arraysCounter = 0;
+
+        Task.Run(() =>
+        {
+            while (stopUi == false)
+            {
+                Thread.Sleep(500);
+                Console.SetCursorPosition(0, 1);
+                Console.Write($"{arraysCounter} / {casesCount}");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Test is completed. Press Enter");
+        });
+
+        foreach (var array in brutforceArrays)
+        {
+            var str = new string(array);
+
+            var hashLength = hashCounter.GetLength(str);
+            var dictLength = dictCounter.GetLength(str);
+            if (hashLength != dictLength)
+                throw new Exception(str);
+
+            arraysCounter++;
+        }
+
+        stopUi = true;
 
         Console.ReadLine();
-    }
-
-    private static bool Test(ILengthCounter counter)
-    {
-        var result = true;
-        result &= Test(counter, "abcabcbb", 3);
-        result &= Test(counter, "bbbbb", 1);
-        result &= Test(counter, "pwwkew", 3);
-        result &= Test(counter, "", 0);
-
-        return result;
-    }
-
-    private static bool Test(ILengthCounter counter, string testStr, int rightAnswer)
-    {
-        var calculatedLength = counter.GetLength(testStr);
-        Console.WriteLine($"{calculatedLength} == {rightAnswer} = {calculatedLength == rightAnswer}");
-
-        return rightAnswer == calculatedLength;
     }
 }
